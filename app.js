7edb0addb60647234992fcac65be25279933e899
Guan Junming <guan.junming85@gmail.com>
2024-09-26 21:25:05 +0800

    complete step 1 - 3

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 //1) Define the required variables used to track the state of the game.
 
+let board;
+let turn;
+let winner;
+let tie;
+
 //2) Store cached element references.
+
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.querySelector("#message");
 
 //3) Upon loading, the game state should be initialized, and a function should
 //   be called to render this game state.
+
+function init() {
+  board = Array(9).fill("");
+  turn = "X";
+  winner = false;
+  tie = false;
+  render();
+}
+
+init();
 
 //4) The state of the game should be rendered to the user.
 
